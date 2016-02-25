3ab2b2e3feed435a54f9c41ac39ea46b6765eefe
ebayrunner <michaeluchida@Michaels-MacBook-Pro-2.local>
2016-02-24 16:16:30 -0800

    First commit

--- Coffeeonline.js
+++ Coffeeonline.js
@@ -1,163 +1,124 @@
 /**
- * Created by michaeluchida on 9/14/15.
- *
+ * Created by michaeluchida on 1/26/16.
  */
 
 
 
-$.getAjax = function(){
-
-    return $.ajax({
-        url: 'package.json',
-        //data: {espressoDrink},
-        dataType:'json'
-
-    }).promise()
-
-
-
+//onChange, contains three callback functions
+var calculateTotal = function(){
+    var totalCost = insideEspressoDrink() + insideEspressoShot() + ice();
+    //assign the reference to the Element object
+    var divobject = document.getElementById("grandTotal");
+    divobject.innerHTML = "$"+totalCost.toFixed(2);
 };
 
+var insideEspressoDrink = function(){
+    var priceOfDrink;
+    //assign the reference to the Element object
+    var whatDrink= document.getElementById("whatDrink")
+    //looks up the espressoDrink object on Price.js
+    priceOfDrink= espressoDrink[whatDrink.value]
+    //returns the price of the drink to calculateTotal
+    return priceOfDrink;
+      };
 
 
-
-
-
-//This is the callback function whos value is returned from the AJAX call
-
-function insideEspressoDrink(jsonData) {
-    var drink = 0;
-
-    return function (jsonData) {
-
-        drink = jsonData.espressoDrink[espressoDrink.value];
-        var theForm = document.forms["coffeeOrderForm"];
-        var divobject = document.getElementById('coffeePrice');
-        divobject.style.display = 'block';
-        divobject.innerHTML = drink;
-        return drink;
-    }
-
-
-}
-
-
-//This is the another callback function whos value is returned from the AJAX call
-
-function insideEspressoShot (jsonData){
-    var shots =0;
-    var theForm = document.forms["coffeeOrderForm"];
-    //var selectedDrink = theForm.elements["numberOfShots"];
-    //set cakeFilling Price equal to value user chose
-    //For example filling_prices["Lemon".value] would be equal to 5
-    shots= jsonData.espressoShot[numberOfShots.value];
-    if((typeof shots)=='string'){
-        //console.log('here is the the type of shots',typeof shots)
-        var divobject = document.getElementById('shotPrice');
-        divobject.style.display='block';
-        divobject.innerHTML = shots;
-        return shots}
+var insideEspressoShot = function(){
+    var totalShots =0;
+    //assign the reference to the Element object
+    var numberOfShots= document.getElementById("numberOfShots")
+    //looks up the howManyShots object on Price.js
+    totalShots = howManyShots[numberOfShots.value];
+    //returns the price of the shots to calculateTotal
+    return totalShots;
 };
 
+ var cupSize = function(){
 
+     var ounceCup=0;
+     //var theForm = document.forms["coffeeOrderForm"];
+     var selectedCup = document.getElementById("sizeOfCup");
+    //there is no price difference for cup size so no price lookup;
+     var divobject = document.getElementById('cupPrice');
+     divobject.innerHTML = selectedCup.value;
+     //the value of ounceCup is returned to calculateTotal
+     return selectedCup.value;
+ };
 
-
-
-
-
-
- function cupSize()
- {
-
-     var cup_price = new Array();
-     cup_price["8"]=0
-     cup_price["12"]=1
-     cup_price ["16"]=2
-     cup_price["20"]=3;
-
- var ounceCup=0;
- //Get a reference to the form id="cakeform"
- var theForm = document.forms["coffeeOrderForm"];
- //Get a reference to the cake the user Chooses name=selectedCake":
- var selectedCake = theForm.elements["sizeOfCup"];
- //Here since there are 4 radio buttons selectedCake.length = 4
- //We loop through each radio buttons
-
- //we set cakeSizePrice to the value of the selected radio button
- //i.e. if the user choose the 8" cake we set it to 25
- //by using the cake_prices array
- //We get the selected Items value
- //For example cake_prices["Round8".value]"
- ounceCup = cup_price[sizeOfCup.value];
-
-
-    var divobject = document.getElementById('cupPrice');
-    divobject.style.display='block';
-    divobject.innerHTML = ounceCup;
-    return ounceCup
- }
-
-
-function icedPrice()
-{
+var ice= function(){
     var icePrice=0;
-    //Get a reference to the form id="cakeform"
-    var theForm = document.forms["coffeeOrderForm"];
-    //Get a reference to the checkbox id="includecandles"
-    var ice = theForm.elements["iced"];
-
-    //If they checked the box set candlePrice to 5
-    if(ice.checked==true)
-    {
-        icePrice=.50;
+    //assign te reference to the Element object
+    var iceChoice = document.getElementById("icedDrink");
+    //A simple boolean. if true add $.75, if false icePrice = 0;
+    if(iceChoice.checked){
+        icePrice = 0.75;
     }
-    //finally we return the candlePrice
     return icePrice;
 }
+//The Google Maps Geolocation API returns a location and accuracy radius
+// based on information about cell towers and WiFi nodes that the mobile client can detect.
+function initMap(){
+    var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 13
+        });
+            //hide the div until the map is rendered
+            $(function () {
+                $("#api_wrapper").show()
 
+            });
+        //infoWindo is the pop-up window indicating latitude and longitude and arrow pointing to rendered map
+        var infoWindow = new google.maps.InfoWindow({map: map});
 
+        // navigator.geolocation determines geo location via data collection mechanisms like the ip of a wifi router, cell tower, etc
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                console.log('from inside initMap', pos)
+                //since (navigator.geolocation) is asych, call whatEver after pos is determined.
+                whatEver(pos);
 
-function commuteMug(){
-    var reusableMug=0;
-    var theForm=document.forms["orderform"];
-    var includeMug = theForm.elements['commutermug'];
-    if(includeMug.checked==true){
-        includeMug=15.75;
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Here is your location. Your latitude is ' + pos.lat.toFixed(4) + ' and your logitude is ' + pos.lng.toFixed(4));
+                map.setCenter(pos);
+            }, function () {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
     }
-    return includeMug;
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
 }
+//google.maps.event.addDomListener(window, "load", initialize);
 
 
-
-//This function invokes the AJAX with callback as its argument
-
-function calculateTotal() {
-    var totalCost;
-    totalCost=  $.getAjax().done(insideEspressoDrink)
-   console.log(totalCost)
-
-
-    var theForm = document.forms["coffeeOrderForm"];
-    var divobject = document.getElementById('grandTotal');
-    divobject.style.display='block';
-    divobject.innerHTML = totalCost;
-};
-
-
-
-
-/*
-
-function hideTotal() {
-    var divobj = document.getElementById('grandTotal');
-    divobj.style.display='none';
-}
-*/
-
-
-
-/*
-
-
-
- *!/*/
+    //this function, with the geolocation coordinates from initMap, makes a JSON request to OpenWeatherMap API.  A JSON file is returned
+    function whatEver(coordinates) {
+        //convert the argument to a format that will work with the api request
+        var latitude = coordinates.lat;
+        var longitude = coordinates.lng;
+        var text = "http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=7534f5fab6676373430402880c728290&";
+        var addCoordinates = "lat=" + latitude + "&lon=" + longitude;
+        var weatherObject = document.getElementById("weatherInfo");
+        //make the ajax call to the API
+        $(function(){
+            $.getJSON(text + addCoordinates,
+                function (data) {
+                    //present to the user the raw JSON file
+                    weatherObject.innerHTML = JSON.stringify(data)
+                    var sortWeather = document.getElementById("sorted_weather");
+                    //present to the user a simple illustration of key:value pairs from the object
+                    sortWeather.innerHTML = "The temperature in " + data.name + " is " + data.main.temp + " degrees, " + " barometric temperature is "
+                        + data.main.pressure + "mmHg, " + " and humidity is " + data.main.humidity + " %."
+                })
+        })
+    }
